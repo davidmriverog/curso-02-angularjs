3ea425b1f98a81ab335d9e71e2dc7ee5d8bdc694
David M. Rivero G <davidmriverog@gmail.com>
2016-01-25 12:35:08 -0430

    finalizacion del ejercicio nro 8
    se finaliza el ejemplo del uso de rutas y vistas añadiendo sus respectivos
    controladores para el dinamismo de dicha pagina.

--- curso2-eje-008/app.js
+++ curso2-eje-008/app.js
@@ -2,7 +2,7 @@ angular.module("MyApp",['ngRoute'])
   .config(function($routeProvider){
     $routeProvider
 	  .when('/',{
-	    templateUrl: 'inicio.html',
+	    templateUrl: 'inicio.html'
 	  })
 	  .when('/tareas',{
 	    templateUrl: 'tareas.html',
@@ -10,10 +10,52 @@ angular.module("MyApp",['ngRoute'])
 	  })
 	  .when('/empleados',{
 	    templateUrl: 'empleados.html',
+		controller: 'FormEmpleadosController'
 	  })
 	  .otherwise({
 	    redirectTo: '/'
 	  });
+  })
+  .controller("FormEmpleadosController",function($scope){
+    $scope.empleados = [
+       {
+        nombre: "David Miguel",
+        apellidos: "Rivero Guite",
+        primerDia: new Date(),
+        salario: 45000,
+        telefono: "424 5449794",
+        bono: 30.4
+       },
+       {
+        nombre: "Yrma",
+        apellidos: "Guite",
+        primerDia: new Date(),
+        salario: 20000,
+        telefono: "424 5449794",
+        bono: 15
+       },
+       {
+        nombre: "Davidmar",
+        apellidos: "Rivero Guite",
+        primerDia: new Date(),
+        salario: 60000,
+        telefono: "424 5449794",
+        bono: 30
+       },
+       {
+        nombre: "David Jonathan",
+        apellidos: "Rivero",
+        primerDia: new Date(),
+        salario: 85000,
+        telefono: "424 5449794",
+        bono: 20
+       }
+     ];
+
+     $scope.ordernarPor = function(criterio){
+
+        $scope.ordenSeleccionado = criterio;
+     };
   })
   .controller("FormTareasController",function($scope){
      $scope.tareas = [
@@ -30,4 +72,39 @@ angular.module("MyApp",['ngRoute'])
        hecho:true
      }
     ];
+	$scope.addTarea = function(){
+      $scope.tareas.push(
+        {
+          texto: $scope.textoNuevaTarea,
+          hecho: false
+        }
+      );
+
+      $scope.textoNuevaTarea = "";
+    };
+
+    $scope.restantes = function(){
+      var cuanta = 0;
+
+      // contador de tareas ya finalizadas.
+      angular.forEach($scope.tareas,function(tarea){
+        cuanta += tarea.hecho ? 0 : 1;
+      });
+
+      return cuanta;
+    };
+
+    $scope.removerTarea = function(){
+      var tareasViejas = $scope.tareas;
+
+      $scope.tareas = [];
+
+      angular.forEach(tareasViejas,function(tarea){
+        if(!tarea.hecho){
+          $scope.tareas.push(tarea);
+        }
+      });
+    };
+	
+	
   });
