683aba9ce982b550274e5ae45797fad687f3e6b4
David Rivero <davidmriverog@gmail.com>
2016-01-25 20:31:20 -0430

    Finalizacion del ejercicio nro. 9
    se finaliza dicho ejercicio lo cual fue integrar las funcionalidades primarias para las directivas con el fin de renderizar nuestras etiquetas html.

--- curso2-eje-009/app.js
+++ curso2-eje-009/app.js
@@ -2,16 +2,24 @@ angular.module('MyApp', [])
   .directive('miEncabezado',function(){
 
     return {
+      restrict: 'E',
       templateUrl: 'mi-encabezado.html',
+      scope: {
+        textoVariable: '=texto',
+
+      }
 
     };
   })
   .controller('AppController',function($scope){
 
-    $scope.texto = {
-      titulo: 'Mi titulo',
+    $scope.texto1 = {
+      titulo: 'Mi titulo 1',
       subtitulo: 'David Rivero'
     };
 
-
+     $scope.texto2 = {
+      titulo: 'Mi titulo 2',
+      subtitulo: 'David Rivero'
+    };
   });
